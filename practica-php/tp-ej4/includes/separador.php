9d600513da8eb096d23077688e153b8bfd8bb802
Fernando Rodriguez <32549106+FerRodriguez96@users.noreply.github.com>
2023-04-28 15:30:43 -0300

    tp3-ej4

---
+++ practica-php/tp-ej4/includes/separador.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if (isset($_POST['numero'])) {
+	$numero = $_POST['numero'];
+    // Se convierte el numero ingresado en un array
+    $digitos = str_split($numero);
+    // Se obtienen la cantidad de digitos que tiene el array
+    $cantidadDigitos = count($digitos);
+    // Se recorre el array
+    for ($i = 0; $i < $cantidadDigitos; $i++) {
+        // Se calcula el exponente de 10 correspondiente a la posicion del array
+        $exponente = $cantidadDigitos - $i - 1;
+        // El valor es igual a la posicion del array por 10 elevado al exponente anterior
+        $valor = $digitos[$i] * pow(10, $exponente);
+        // Se imprime el valor
+        echo $valor . " ";
+        // Se agrega un esparador
+        if ($i < $cantidadDigitos - 1) {
+            echo "- ";
+        }
+    }
+}
+?>
+</body>
+</html>
